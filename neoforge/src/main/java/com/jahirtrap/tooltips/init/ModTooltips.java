c8e1ecbb1687e7ad8abd6fba1f1204b11613f100
jahirxtrap <jahirroman2002@gmail.com>
2025-03-03 01:35:02 -0500

    v1.0.2

--- neoforge/src/main/java/com/jahirtrap/tooltips/init/ModTooltips.java
+++ neoforge/src/main/java/com/jahirtrap/tooltips/init/ModTooltips.java
@@ -116,11 +116,9 @@ public class ModTooltips {
         }
 
         if (ModConfig.showModName) {
-            var modContainer = ModList.get().getModContainerById(BuiltInRegistries.ITEM.getKey(stack.getItem()).getNamespace());
-            if (modContainer.isPresent()) {
-                Component modNameTooltip = Component.literal(modContainer.get().getModInfo().getDisplayName()).withStyle(ChatFormatting.BLUE, ChatFormatting.ITALIC);
-                list.add(modNameTooltip);
-            }
+            String modId = BuiltInRegistries.ITEM.getKey(stack.getItem()).getNamespace();
+            Component modNameTooltip = Component.literal(ModList.get().getModContainerById(modId).map(container -> container.getModInfo().getDisplayName()).orElse(modId)).withStyle(ChatFormatting.BLUE, ChatFormatting.ITALIC);
+            list.add(modNameTooltip);
         }
     }
 
