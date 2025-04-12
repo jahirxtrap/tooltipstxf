43f6ab8d84862fa0915aeb3191634e3cd5391bd4
jahirxtrap <jahirroman2002@gmail.com>
2025-04-11 23:02:57 -0500

    v1.0.3

--- fabric/src/main/java/com/jahirtrap/tooltips/init/ModTooltips.java
+++ fabric/src/main/java/com/jahirtrap/tooltips/init/ModTooltips.java
@@ -24,7 +24,7 @@ public class ModTooltips {
         if (ModConfig.showDurability) {
             if ((!flag.isAdvanced() || (flag.isAdvanced() && !stack.isDamaged())) && stack.getMaxDamage() != 0) {
                 Component durabilityTooltip = new TranslatableComponent("item.durability", stack.getMaxDamage() - stack.getDamageValue(), stack.getMaxDamage());
-                list.add(durabilityTooltip.copy().withStyle(ChatFormatting.DARK_GRAY));
+                list.add(durabilityTooltip.copy().withStyle(s -> s.withColor(getColor(0x555555, ModConfig.durabilityColor))));
             }
         }
 
@@ -41,14 +41,14 @@ public class ModTooltips {
                 else if (saturation != 0)
                     foodTooltip = new TranslatableComponent("tooltipstxf.tooltip.saturation", formatText(saturation));
                 if (foodTooltip != null)
-                    list.add(foodTooltip.copy().withStyle(ChatFormatting.DARK_GRAY));
+                    list.add(foodTooltip.copy().withStyle(s -> s.withColor(getColor(0x555555, ModConfig.foodValuesColor))));
             }
         }
 
         if (ModConfig.showCompostable) {
             float compostable = ComposterBlock.COMPOSTABLES.getOrDefault(stack.getItem(), 0);
             if (compostable != 0) {
-                Component compostableTooltip = new TranslatableComponent("tooltipstxf.tooltip.compostable", formatText(compostable * 100)).append("%").withStyle(ChatFormatting.DARK_GRAY);
+                Component compostableTooltip = new TranslatableComponent("tooltipstxf.tooltip.compostable", formatText(compostable * 100)).append("%").withStyle(s -> s.withColor(getColor(0x555555, ModConfig.compostableColor)));
                 list.add(compostableTooltip);
             }
         }
@@ -61,7 +61,7 @@ public class ModTooltips {
                     burnTimeTooltip = new TranslatableComponent("tooltipstxf.tooltip.burn_time.seconds", formatText(burnTime / 20f));
                 else
                     burnTimeTooltip = new TranslatableComponent("tooltipstxf.tooltip.burn_time", burnTime);
-                list.add(burnTimeTooltip.copy().withStyle(ChatFormatting.DARK_GRAY));
+                list.add(burnTimeTooltip.copy().withStyle(s -> s.withColor(getColor(0x555555, ModConfig.burnTimeColor))));
             }
         }
 
@@ -73,14 +73,14 @@ public class ModTooltips {
                     cooldownComponent = new TranslatableComponent("tooltipstxf.tooltip.use_cooldown.seconds", formatText((cooldown.endTime - cooldown.startTime) / 20f));
                 else
                     cooldownComponent = new TranslatableComponent("tooltipstxf.tooltip.use_cooldown", cooldown.endTime - cooldown.startTime);
-                list.add(cooldownComponent.copy().withStyle(ChatFormatting.DARK_GRAY));
+                list.add(cooldownComponent.copy().withStyle(s -> s.withColor(getColor(0x555555, ModConfig.useCooldownColor))));
             }
         }
 
         if (ModConfig.showEnchantability) {
             int enchantable = stack.getItem().getEnchantmentValue();
             if (enchantable != 0) {
-                Component enchantabilityComponent = new TranslatableComponent("tooltipstxf.tooltip.enchantability", enchantable).withStyle(ChatFormatting.DARK_GRAY);
+                Component enchantabilityComponent = new TranslatableComponent("tooltipstxf.tooltip.enchantability", enchantable).withStyle(s -> s.withColor(getColor(0x555555, ModConfig.enchantabilityColor)));
                 list.add(enchantabilityComponent);
             }
         }
@@ -88,7 +88,7 @@ public class ModTooltips {
         if (ModConfig.showRepairCost) {
             int repairCost = stack.getBaseRepairCost();
             if (repairCost != 0)
-                list.add(new TranslatableComponent("tooltipstxf.tooltip.repair_cost", repairCost + 1).withStyle(ChatFormatting.DARK_GRAY));
+                list.add(new TranslatableComponent("tooltipstxf.tooltip.repair_cost", repairCost + 1).withStyle(s -> s.withColor(getColor(0x555555, ModConfig.repairCostColor))));
         }
 
         var block = Block.byItem(stack.getItem());
@@ -102,13 +102,13 @@ public class ModTooltips {
                 else if (block.getExplosionResistance() != 0)
                     strengthTooltip = new TranslatableComponent("tooltipstxf.tooltip.resistance", formatText(block.getExplosionResistance()));
                 if (strengthTooltip != null)
-                    list.add(strengthTooltip.copy().withStyle(ChatFormatting.DARK_GRAY));
+                    list.add(strengthTooltip.copy().withStyle(s -> s.withColor(getColor(0x555555, ModConfig.strengthColor))));
             }
 
             if (ModConfig.showEnchantmentPower && player != null) {
                 float enchantPower = getEnchantPowerBonus(player.getLevel(), block);
                 if (enchantPower != 0) {
-                    Component enchantPowerTooltip = new TranslatableComponent("tooltipstxf.tooltip.enchantment_power", formatText(enchantPower)).withStyle(ChatFormatting.DARK_GRAY);
+                    Component enchantPowerTooltip = new TranslatableComponent("tooltipstxf.tooltip.enchantment_power", formatText(enchantPower)).withStyle(s -> s.withColor(getColor(0x555555, ModConfig.enchantmentPowerColor)));
                     list.add(enchantPowerTooltip);
                 }
             }
@@ -116,17 +116,27 @@ public class ModTooltips {
 
         if (ModConfig.showModName) {
             String modId = Registry.ITEM.getKey(stack.getItem()).getNamespace();
-            Component modNameTooltip = new TextComponent(FabricLoader.getInstance().getModContainer(modId).map(container -> container.getMetadata().getName()).orElse(modId)).withStyle(ChatFormatting.BLUE, ChatFormatting.ITALIC);
+            Component modNameTooltip = new TextComponent(FabricLoader.getInstance().getModContainer(modId).map(container -> container.getMetadata().getName()).orElse(modId)).withStyle(ChatFormatting.ITALIC).withStyle(s -> s.withColor(getColor(0x5555ff, ModConfig.modNameColor)));
             list.add(modNameTooltip);
         }
     }
 
-    public static String formatText(double value) {
+    private static String formatText(double value) {
         if (value % 1 == 0) return String.valueOf((int) value);
         else return String.format("%.2f", value).replaceAll("0*$", "").replaceAll("\\.$", "");
     }
 
     private static float getEnchantPowerBonus(Level level, Block block) {
         return block.defaultBlockState().is(Blocks.BOOKSHELF) ? 1 : 0;
+    }
+
+    private static Integer getColor(int defaultValue, String hexColor) {
+        int color = defaultValue;
+        try {
+            if (hexColor.startsWith("#")) hexColor = hexColor.substring(1);
+            color = Integer.parseInt(hexColor, 16);
+        } catch (Exception ignore) {
+        }
+        return color;
     }
 }
