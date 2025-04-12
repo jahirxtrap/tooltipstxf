5dd69ae8d2ef6763937ff7d30238a20c50da1f10
jahirxtrap <jahirroman2002@gmail.com>
2025-04-11 23:11:40 -0500

    v1.0.3

--- fabric/src/main/java/com/jahirtrap/tooltips/init/ModConfig.java
+++ fabric/src/main/java/com/jahirtrap/tooltips/init/ModConfig.java
@@ -27,4 +27,25 @@ public class ModConfig extends TXFConfig {
     public static boolean showEnchantmentPower = true;
     @Entry(name = "Show Mod Name", itemDisplay = "minecraft:writable_book")
     public static boolean showModName = false;
+    public static Comment colors;
+    @Entry(name = "Durability Color", width = 7, min = 7, isColor = true)
+    public static String durabilityColor = "#555555";
+    @Entry(name = "Food Values Color", width = 7, min = 7, isColor = true)
+    public static String foodValuesColor = "#555555";
+    @Entry(name = "Compostable Color", width = 7, min = 7, isColor = true)
+    public static String compostableColor = "#555555";
+    @Entry(name = "Burn Time Color", width = 7, min = 7, isColor = true)
+    public static String burnTimeColor = "#555555";
+    @Entry(name = "Use Cooldown Color", width = 7, min = 7, isColor = true)
+    public static String useCooldownColor = "#555555";
+    @Entry(name = "Enchantability Color", width = 7, min = 7, isColor = true)
+    public static String enchantabilityColor = "#555555";
+    @Entry(name = "Repair Cost Color", width = 7, min = 7, isColor = true)
+    public static String repairCostColor = "#555555";
+    @Entry(name = "Strength Color", width = 7, min = 7, isColor = true)
+    public static String strengthColor = "#555555";
+    @Entry(name = "Enchantment Power Color", width = 7, min = 7, isColor = true)
+    public static String enchantmentPowerColor = "#555555";
+    @Entry(name = "Mod Name Color", width = 7, min = 7, isColor = true)
+    public static String modNameColor = "#5555ff";
 }
