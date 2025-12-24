1086da6e164becfc2ef5bfb6cda4205d66e4b950
jahirxtrap <jahirroman2002@gmail.com>
2025-12-24 00:26:11 -0500

    1.21.11

--- neoforge/src/main/java/com/jahirtrap/tooltips/init/ModTags.java
+++ neoforge/src/main/java/com/jahirtrap/tooltips/init/ModTags.java
@@ -1,15 +1,15 @@
 package com.jahirtrap.tooltips.init;
 
 import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
 
 public class ModTags {
     public interface Blocks {
-        TagKey<Block> NEEDS_NETHERITE_TOOL = create(ResourceLocation.withDefaultNamespace("needs_netherite_tool"));
+        TagKey<Block> NEEDS_NETHERITE_TOOL = create(Identifier.withDefaultNamespace("needs_netherite_tool"));
 
-        private static TagKey<Block> create(ResourceLocation name) {
+        private static TagKey<Block> create(Identifier name) {
             return TagKey.create(Registries.BLOCK, name);
         }
     }
